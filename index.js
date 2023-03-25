5be9d4cae2f4041ef4b84a60d8848fcc81c1449a
Tigryla <tigrtigrovichyt@gmail.com>
2023-03-25 10:43:40 +0200

    connect data base and link html

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const todosRoutes = require("./routes/todos");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(todosRoutes);
 
 async function start() {
   try {
-    await mongoose.connect("", {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect(
+      "mongodb+srv://tg-app:4339875asd@cluster0.jjyrf7v.mongodb.net/?retryWrites=true&w=majority"
+    );
     app.listen(PORT, () => {
       console.log("Server has been started...");
     });
